36a65ce28bb44f4ceafe569cb6052e3e3dca4fce
jdohm <jannismax@gmail.com>
2021-02-16 02:29:23 +0100

    load saved ideas #10

--- myServer.js
+++ myServer.js
@@ -8,76 +8,102 @@ const port = process.argv[2] || 8081;
 var qs = require('querystring');
 
 http.createServer(function (req, res) {
-  if(req.method === "GET") {
-  console.log(`${req.method} ${req.url}`);
+  if (req.method === "GET") {
+    console.log(`${req.method} ${req.url}`);
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+    if (parsedUrl.pathname == `/getIdeas`) {
 
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-	if(parsedUrl.pathname == `/`) parsedUrl.pathname = `/index.html`;
-  console.log(`parsedUrl: ${parsedUrl.pathname}`);
-  // extract URL path
-  let pathname = `.${parsedUrl.pathname}`;
-  console.log(`parsedname: ${pathname}`);
-  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extention to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
+        let db = new sqlite3.Database('./db/Ideas.db', (err) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        console.log('Connected to the SQlite database.');
+      });
+      //read all places from Database 
+      db.serialize(function() {
+      let sql = `SELECT lon LON , lat LAT, IdeaID IDEAID FROM Places ORDER BY lat`;
 
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
-      return;
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            console.log("lon: " + row.LON + " lat: " + row.LAT + " IdeaID: " + row.IDEAID);
+        });
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.end(JSON.stringify(rows));
+      });
+      db.close();
+      });
     }
+    else {
+      if (parsedUrl.pathname == `/`) parsedUrl.pathname = `/index.html`;
+      console.log(`parsedUrl: ${parsedUrl.pathname}`);
+      // extract URL path
+      let pathname = `.${parsedUrl.pathname}`;
+      console.log(`parsedname: ${pathname}`);
+      // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+      const ext = path.parse(pathname).ext;
+      // maps file extention to MIME typere
+      const map = {
+        '.ico': 'image/x-icon',
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.json': 'application/json',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.wav': 'audio/wav',
+        '.mp3': 'audio/mpeg',
+        '.svg': 'image/svg+xml',
+        '.pdf': 'application/pdf',
+        '.doc': 'application/msword'
+      };
 
-    // if is a directory search for index file matching the extention
-    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+      fs.exists(pathname, function (exist) {
+        if (!exist) {
+          // if the file is not found, return 404
+          res.statusCode = 404;
+          res.end(`File ${pathname} not found!`);
+          return;
+        }
 
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-  } else if(req.method === "POST") {
+        // if is a directory search for index file matching the extention
+        if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+        // read file from file system
+        fs.readFile(pathname, function (err, data) {
+          if (err) {
+            res.statusCode = 500;
+            res.end(`Error getting the file: ${err}.`);
+          } else {
+            // if the file is found, set Content-type and send data
+            res.setHeader('Content-type', map[ext] || 'text/plain');
+            res.end(data);
+          }
+        });
+      });
+    }
+  } else if (req.method === "POST") {
     if (req.url === "/submitIdea") {
       var requestBody = '';
-      req.on('data', function(data) {
+      req.on('data', function (data) {
         requestBody += data;
-        if(requestBody.length > 1e7) {
-          res.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
+        if (requestBody.length > 1e7) {
+          res.writeHead(413, 'Request Entity Too Large', { 'Content-Type': 'text/html' });
           res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
         }
       });
-      req.on('end', function() {
+      req.on('end', function () {
         var formData = qs.parse(requestBody);
         console.log(`formData ${formData.ideaText}`);
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<!doctype html><html><head><title>response</title></head><body>');
-        res.write('Thanks for your Idea<br />'+formData.nameText);
-        res.write('<br />Description: '+formData.ideaText);
-        res.write('<br />Categories: '+formData.categoriesText);
-        res.write('<br />Skills: '+formData.skillsText);
+        res.write('Thanks for your Idea<br />' + formData.nameText);
+        res.write('<br />Description: ' + formData.ideaText);
+        res.write('<br />Categories: ' + formData.categoriesText);
+        res.write('<br />Skills: ' + formData.skillsText);
         res.end('</body></html>');
 
         let db = new sqlite3.Database('./db/Ideas.db', (err) => {
