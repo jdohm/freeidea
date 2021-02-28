3efc1fd90a5acd3a28b3bae6e0be7d81db80df20
Jannis Dohm <jannismax@gmail.com>
2021-02-28 04:16:06 +0100

    solves #31 - migrate node js server to Express
    This is in preparation of user management
    
    .gitignore
    - added .env to ignore list (preparation for user management)
    
    myServer.js
    - refactoring - switched to Express framework
    
    package-lock.json
    - new modules for express added
    
    package.json
    - new modules for express added
    - added command to start nodemon server
      (auto reload on file save while developing)

--- myServer.js
+++ myServer.js
@@ -1,25 +1,20 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
+//myExpressServer.js
+const express = require('express');
 const fs = require('fs');
-const path = require('path');
-//const mysql = require('mysql');
 const sqlite3 = require('sqlite3').verbose();
+const https = require('https');
+var path = require('path');
+const app = express();
+const http = express();
 const port = process.argv[2] || 80;
 const portSSL = process.argv[3] || 443;
-var qs = require('querystring');
 
-http.createServer(function (req, res){
-          res.end('<!doctype html><html><head><title>413</title></head><body>301: redirect to https</body><script> console.log("you are accessing us via " +  "an insecure protocol (HTTP). " + "Redirecting you to HTTPS."); window.location.href =  window.location.href.replace("http:", "https:"); </script></html>');
-}).listen(parseInt(port));
-console.log(`http Server listening on port ${port}`);
-
+//options for https server
 var options = {
     key: fs.readFileSync('/etc/letsencrypt/live/freeidea.de/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/freeidea.de/cert.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/live/freeidea.de/chain.pem')
-}
-
+};
 
 // Start sqlite3 database connection
 let db = new sqlite3.Database('./db/Ideas.db', (err) => {
@@ -29,267 +24,139 @@ let db = new sqlite3.Database('./db/Ideas.db', (err) => {
     console.log('Connected to the SQlite database.');
 });
 
-https.createServer(options, function (req, res) {
-  if (req.method === "GET") {
-    console.log(`${req.method} ${req.url}`);
-    // parse URL
-    const parsedUrl = url.parse(req.url);
-    if (parsedUrl.pathname == `/getIdeas`) {
+//make URL encoded data accessible inside get/post function
+app.use(express.urlencoded({ extended: false}));
 
-      //read all places from Database 
-      db.serialize(function() {
-          let sql = `SELECT lon, lat, IdeaID, upvotes, downvotes FROM v_PlacesVotes ORDER BY lat`;
+//app startpage
+app.get('/', function (req, res) {
+    // res.send(fs.readFileSync('./index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-      db.all(sql, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        rows.forEach((row) => {
-            console.log("lon: " + row.lon + " lat: " + row.lat + " IdeaID: " + row.IdeaID + " upvotes: " + row.upvotes + " downvotes: " + row.downvotes);
+//respond to getIdeas GET request
+app.get('/getIdeas', function (req, res) {
+    //read all places from Database 
+    db.serialize(function() {
+        let sql = `SELECT lon, lat, IdeaID, upvotes, downvotes FROM v_PlacesVotes ORDER BY lat`;
+        db.all(sql, [], (err, rows) => {
+            if (err) throw err;
+            res.json(rows);
         });
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.end(JSON.stringify(rows));
-      });
-      });
-    }
-    else {
-      if (parsedUrl.pathname == `/`) parsedUrl.pathname = `/index.html`;
-      console.log(`parsedUrl: ${parsedUrl.pathname}`);
-      // extract URL path
-      let pathname = `.${parsedUrl.pathname}`;
-      console.log(`parsedname: ${pathname}`);
-      // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-      const ext = path.parse(pathname).ext;
-      // maps file extention to MIME typere
-      const map = {
-        '.ico': 'image/x-icon',
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.json': 'application/json',
-        '.css': 'text/css',
-        '.png': 'image/png',
-        '.jpg': 'image/jpeg',
-        '.wav': 'audio/wav',
-        '.mp3': 'audio/mpeg',
-        '.svg': 'image/svg+xml',
-        '.pdf': 'application/pdf',
-        '.doc': 'application/msword'
-      };
+    });
+});
 
-      fs.exists(pathname, function (exist) {
-        if (!exist) {
-          // if the file is not found, return 404
-          res.statusCode = 404;
-          res.end(`File ${pathname} not found!`);
-          return;
-        }
-
-        // if is a directory search for index file matching the extention
-        if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-        // read file from file system
-        fs.readFile(pathname, function (err, data) {
-          if (err) {
-            res.statusCode = 500;
-            res.end(`Error getting the file: ${err}.`);
-          } else {
-            // if the file is found, set Content-type and send data
-            res.setHeader('Content-type', map[ext] || 'text/plain');
-            res.end(data);
-          }
-        });
-      });
-    }
-  } else if (req.method === "POST") {
-    if (req.url === "/submitIdea") {
-      var requestBody = '';
-      req.on('data', function (data) {
-        requestBody += data;
-        if (requestBody.length > 1e7) {
-          res.writeHead(413, 'Request Entity Too Large', { 'Content-Type': 'text/html' });
-          res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
-        }
-      });
-      req.on('end', function () {
-        var formData = qs.parse(requestBody);
-        console.log(`tags: ${formData.tags}`);
-        console.log(`Skills: ${formData.skills}`);
-        console.log(`Idea created title: ${formData.ideaText}`);
-
-        //Write to Database (use in POST answer to save data)
-        db.serialize(function () {
-          var lastID;
-          db.run('INSERT INTO Idea(title,description) VALUES(?1,?2)', {
-            1: formData.nameText,
-            2: formData.ideaText
-          }, function (err) {
-            if (err) {
-              return console.log(err.message);
-            }
-            // get the last insert id
-            lastID = this.lastID;
-            console.log(`A row has been inserted into Idea with rowid ${this.lastID}`);
-            db.run('INSERT INTO Places(lon,lat,IdeaID) VALUES(?1,?2,?3)', {
-              1: formData.lon,
-              2: formData.lat,
-              3: this.lastID
-            }, function (err) {
-              if (err) {
-                return console.log(err.message);
-              }
-              // get the last insert id
-              console.log(`A row has been inserted with rowid ${this.lastID}`);
-            });
-              var tags = formData.tags.split(",");
-              tags.forEach( function(item,index) {
-                db.run('INSERT OR IGNORE INTO Tags(Name) VALUES(?1)', {
-                    1: item.trim()
-                }, function (err) {
-                    if (err) {
-                        return console.log(err.message);
-                    }
-                    db.run('INSERT OR IGNORE INTO Idea_Tags(Idea,Tag) VALUES(?1, ?2)', {
-                        1: lastID,
-                        2: item.trim()
-                    }, function (err) {
-                        if (err) {
-                            return console.log(err.message);
-                        }
-                    });
-                });
-              });
-                  var skills = formData.skills.split(",");
-                  skills.forEach( function(item,index) {
-                      db.run('INSERT OR IGNORE INTO Skills(Name) VALUES(?1)', {
-                          1: item.trim()
-                      }, function (err) {
-                          if (err) {
-                              return console.log(err.message);
-                          }
-                          db.run('INSERT OR IGNORE INTO Idea_Skills(Idea,Skill) VALUES(?1, ?2)', {
-                              1: lastID,
-                              2: item.trim()
-                          }, function (err) {
-                              if (err) {
-                                  return console.log(err.message);
-                              }
-                          });
-                      });
-                  });
-              res.writeHead(200, {'Content-Type': 'application/json'});
-              res.end(JSON.stringify(lastID));
-          });
-        });
-      });
-    }
-      else if (req.url == `/getIdea`) {
-      var requestBody = '';
-      //prevent big fileupload
-      req.on('data', function (data) {
-        requestBody += data;
-        if (requestBody.length > 1e7) {
-          res.writeHead(413, 'Request Entity Too Large', { 'Content-Type': 'text/html' });
-          res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
-        }
-      });
-      req.on('end', function () {
-        var formData = qs.parse(requestBody);
-        console.log(`formData ${formData.IdeaID}`);
-
-        //Write to Database (use in POST answer to save data)
+//respond to getIdea POST request
+app.post('/getIdea', function (req, res) {
+    if(!req.body.IdeaID) res.json();
+    else{
+        //Read idea data from database
         db.serialize(function () {
             let sql = `SELECT ID AskForIdea, title TITLE, description DESCRIPTION FROM Idea WHERE ID is ?`;
-            db.get(sql, [formData.IdeaID], (err, rows) => {
-                if (err) {
-                    throw err;
-                }
+            db.get(sql, [req.body.IdeaID], (err, rows) => {
+                if (err) throw err;
                 //if IdeaID was not found, cancel following
                 if(!rows) return 0;
                 var idearows = rows;
                 let sql = `SELECT Tag FROM Idea_Tags WHERE Idea is ?`;
-                db.all(sql, [formData.IdeaID], (err, rows) => {
-                    if (err) {
-                        throw err;
-                    }
+                db.all(sql, [req.body.IdeaID], (err, rows) => {
+                    if (err) throw err;
                     var tags = [];
                     rows.forEach((row) => {tags.push(row.Tag);});
                     idearows.tags = tags;
                     let sql = `SELECT Skill FROM Idea_Skills WHERE Idea is ?`;
-                    db.all(sql, [formData.IdeaID], (err, rows) => {
-                        if (err) {
-                            throw err;
-                        }
+                    db.all(sql, [req.body.IdeaID], (err, rows) => {
+                        if (err) throw err;
                         var crd = [];
                         rows.forEach((row) => {crd.push(row.Skill);});
                         idearows.skills = crd;
-                        res.writeHead(200, {'Content-Type': 'application/json'});
-                        console.log(JSON.stringify(idearows));
-                        res.end(JSON.stringify(idearows));
+                        res.json(idearows);
                     });
                 });
             });
         });
-      });
-      }
-      else if (req.url == `/submitVote`) {
-      var requestBody = '';
-      //prevent big fileupload
-      req.on('data', function (data) {
-        requestBody += data;
-        if (requestBody.length > 1e7) {
-          res.writeHead(413, 'Request Entity Too Large', { 'Content-Type': 'text/html' });
-          res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
-        }
-      });
-      req.on('end', function () {
-        var formData = qs.parse(requestBody);
-          var formDataUp;
-          if(formData.upvote == 1) formDataUp = "Up";
-          else formDataUp = "Down";
-          console.log(`formData ${formData.IdeaID} vote` + formDataUp);
-
-        db.serialize(function () {
-            // new Date object
-            let date_ob = new Date();
-            //console.log(date_ob.getFullYear() + "-" + ("0" + (date_ob.getMonth() + 1)).slice(-2) + "-" + ("0" + date_ob.getDate()).slice(-2) + " " + ("0" + date_ob.getHours()).slice(-2) + ":" +("0" + date_ob.getMinutes()).slice(-2) + ":" + ("0" + date_ob.getSeconds()).slice(-2));
-            db.run('INSERT INTO Votes(IdeaID,upvote,DateTime) VALUES(?1,?2,?3)', {
-                1: formData.IdeaID,
-                2: formData.upvote,
-                3: date_ob.getFullYear() + "-" + ("0" + (date_ob.getMonth() + 1)).slice(-2) + "-" + ("0" + date_ob.getDate()).slice(-2) + " " + ("0" + date_ob.getHours()).slice(-2) + ":" +("0" + date_ob.getMinutes()).slice(-2) + ":" + ("0" + date_ob.getSeconds()).slice(-2)
-            }, function (err) {
-                if (err) {
-                    return console.log(err.message);
-                }
-            });
-        });
-      });
-          res.end();
-      }
-      else {
-      res.writeHead(404, 'Resource Not Found', { 'Content-Type': 'text/html' });
-      res.end('<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>');
     }
-  } else {
-    res.writeHead(405, 'Method Not Supported', { 'Content-Type': 'text/html' });
-    return res.end('<!doctype html><html><head><title>405</title></head><body>405: Method Not Supported</body></html>');
-  }
-}).listen(parseInt(portSSL));
-console.log(`Server listening on port ${portSSL}`);
-
-
-//graceful shutdown source: https://medium.com/hackernoon/graceful-shutdown-in-nodejs-2f8f59d1c357
-process.on('SIGTERM', () => {
-    console.info('SIGTERM signal received.');
-        db.close((err) => {
-            console.log('SQLite connection closed.');
-            process.exit(0);
-        });
 });
 
-process.on('SIGINT', () => {
-    console.info('SIGINT signal received.');
-        db.close((err) => {
-            console.log('SQLite connection closed.');
-            process.exit(0);
+//receive votes from client and save them in db
+app.post('/submitVote', function (req, res) {
+    db.serialize(function () {
+        let date_ob = new Date();
+        //datenow is date formated as string like this: YYYY-MM-DD HH:mm:ss
+        let datenow = date_ob.getFullYear() + "-" + ("0" + (date_ob.getMonth() + 1)).slice(-2) + "-" + ("0" + date_ob.getDate()).slice(-2) + " " + ("0" + date_ob.getHours()).slice(-2) + ":" +("0" + date_ob.getMinutes()).slice(-2) + ":" + ("0" + date_ob.getSeconds()).slice(-2);
+        db.run('INSERT INTO Votes(IdeaID,upvote,DateTime) VALUES(?1,?2,?3)', {
+            1: req.body.IdeaID,
+            2: req.body.upvote,
+            3: datenow
+            }, function (err) {if (err) return console.log(err.message);
+                               res.send("vote saved");
         });
+    });
 });
+
+app.post('/submitIdea', function (req, res) {
+    db.serialize(function () {
+        var lastID;
+        db.run('INSERT INTO Idea(title,description) VALUES(?1,?2)', {
+        1: req.body.nameText,
+        2: req.body.ideaText
+        }, function (err) {
+        if (err) return console.log(err.message);
+        lastID = this.lastID;
+        db.run('INSERT INTO Places(lon,lat,IdeaID) VALUES(?1,?2,?3)', {
+            1: req.body.lon,
+            2: req.body.lat,
+            3: this.lastID
+        }, function (err) {
+            if (err) return console.log(err.message);
+        });
+            var tags = req.body.tags.split(",");
+            tags.forEach( function(item,index) {
+            db.run('INSERT OR IGNORE INTO Tags(Name) VALUES(?1)', {
+                1: item.trim()
+            }, function (err) {
+                if (err) return console.log(err.message);
+                db.run('INSERT OR IGNORE INTO Idea_Tags(Idea,Tag) VALUES(?1, ?2)', {
+                    1: lastID,
+                    2: item.trim()
+                }, function (err) {
+                    if (err) return console.log(err.message);
+                });
+            });
+            });
+            var skills = req.body.skills.split(",");
+            skills.forEach( function(item,index) {
+                db.run('INSERT OR IGNORE INTO Skills(Name) VALUES(?1)', {
+                    1: item.trim()
+            }, function (err) {
+                if (err) return console.log(err.message);
+                db.run('INSERT OR IGNORE INTO Idea_Skills(Idea,Skill) VALUES(?1, ?2)', {
+                    1: lastID,
+                    2: item.trim()
+                }, function (err) {
+                    if (err) return console.log(err.message);
+                });
+            });
+            });
+            res.json(lastID);
+        });
+    });
+});
+
+//serve static content
+app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/media', express.static(path.join(__dirname, 'media')));
+
+
+
+
+
+//http to https redirect
+http.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+});
+
+
+//start server and listen to ports
+http.listen(port, function() {console.log('http server running at port: ' + port);});
+https.createServer(options, app).listen(portSSL, function() {console.log('https server running at port: ' + portSSL);});
